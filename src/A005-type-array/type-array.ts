b897b8a9be7a65972ab7af07dae0521c429f2a55
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-12-11 12:15:10 -0300

    type tuple

--- src/A005-type-array/type-array.ts
+++ src/A005-type-array/type-array.ts
@@ -1,7 +1,7 @@
 //Array<T>  >>> array[]
 
 function multiplyArgs(...args: Array<number>) {
-  return args.reduce((mult, valor) => mult * valor, 1);
+    return args.reduce((mult, valor) => mult * valor, 1);
 }
 
 const result = multiplyArgs(1, 2, 3);
---
+++ src/A006-type-tuple/A006-type-tuple.ts
@@ -0,0 +1,5 @@
+const client: [number, string] = [1, 'jonathan'];
+
+client[0] = 100;
+client[1] = 'jonas';
+console.log(client);
