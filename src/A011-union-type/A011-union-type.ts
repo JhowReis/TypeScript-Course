6c25370c63a8bc0448f9e9ad53e71c446725ab5b
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-12-16 17:26:33 -0300

    union type

---
+++ src/A011-union-type/A011-union-type.ts
@@ -0,0 +1,6 @@
+function sumOrConcat(a: number | string, b: number | string): number | string {
+    if (typeof a === 'number' && typeof b === 'number') return a + b;
+    return `${a}${b}`;
+}
+console.log(sumOrConcat(10, 20));
+console.log(sumOrConcat('JONATHAN', 'REIS'));
